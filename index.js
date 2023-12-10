fa0511c3114b413777019f0d4e50faf927bb5fc6
Alwi Rihad <alwirihad8@gmail.com>
2023-12-10 22:36:25 +0700

    modified:   .gitignore 	modified:   index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const { setupRoutes } = require('./routes/foodController/food.router');
 const axios = require('axios');
+const path = require('path')
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,16 +12,16 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 
-// New endpoint to consume Flask API
-app.post('/get_recommendations', async (req, res) => {
+const csvPath = process.env.CSV_PATH || path.join(__dirname, 'default_path', 'recido.csv');
+
+app.post('/recommend', async (req, res) => {
   const inputData = {
-    title: req.body.title,
     ingredients: req.body.ingredients
   };
 
   try {
     // Make a request to Flask API
-    const response = await axios.post('http://localhost:3000/get_recommendations', inputData);
+    const response = await axios.post('http://127.0.0.1:5000/recommend', inputData);
 
     // Send the response from Flask API to the client
     res.status(200).json(response.data);
